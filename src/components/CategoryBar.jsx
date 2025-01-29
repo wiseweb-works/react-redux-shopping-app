669ef3fffea127a58bd6b17f3396984cf5328802
Wise <wisewebworks@outlook.com>
2025-01-30 00:34:16 +0100

    Components work fully with extraReducers

--- src/components/CategoryBar.jsx
+++ src/components/CategoryBar.jsx
@@ -1,53 +1,35 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import {
   Radio,
   RadioGroup,
   FormControlLabel,
   FormControl,
 } from '@mui/material/';
-import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  setLoading,
-  setProducts,
   setSelected,
+  getCategories,
+  fetchWithCategorie,
+  fetchProducts,
 } from '../redux/reducer/productReducer';
 
 const CategoryBar = () => {
-  const [categories, setCategories] = useState([]);
   const dispatch = useDispatch();
-  const { selected } = useSelector((state) => state.product);
+  const { selected, categories } = useSelector((state) => state.product);
 
   useEffect(() => {
-    getCategories();
-  }, []);
-
-  const getCategories = () => {
-    const baseUrl = 'https://fakestoreapi.com/products/categories';
-    axios
-      .get(baseUrl)
-      .then((res) => {
-        setCategories(res.data);
-      })
-      .catch((e) => console.log(e));
-  };
-
-  const getProducts = (url = '') => {
-    const baseUrl = 'https://fakestoreapi.com/products';
-    dispatch(setLoading(true));
-    axios
-      .get(baseUrl + url)
-      .then((res) => {
-        dispatch(setProducts(res.data));
-      })
-      .catch((e) => console.log(e))
-      .finally(() => dispatch(setLoading(false)));
-  };
+    dispatch(getCategories());
+  }, [dispatch]);
 
   const handleClick = (e) => {
     dispatch(setSelected(e.target.value));
-    getProducts(e.target.value && `/category/${e.target.value}`);
+    if (e.target.value) {
+      dispatch(fetchWithCategorie(e.target.value));
+    } else {
+      dispatch(fetchProducts());
+    }
   };
+
   return (
     <FormControl sx={{ width: '100vw', m: 1 }}>
       <RadioGroup
--- src/redux/reducer/productReducer.js
+++ src/redux/reducer/productReducer.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 const initialState = {
   selected: '',
   products: [],
+  categories: [],
   loading: false,
 };
 
@@ -11,6 +12,26 @@ export const fetchProducts = createAsyncThunk(
   'product/fetchProducts',
   async () => {
     const response = await axios.get('https://fakestoreapi.com/products');
+    return response.data;
+  }
+);
+
+export const fetchWithCategorie = createAsyncThunk(
+  'product/fetchWithCategorie',
+  async (categorie) => {
+    const response = await axios.get(
+      `https://fakestoreapi.com/products/category/${categorie}`
+    );
+    return response.data;
+  }
+);
+
+export const getCategories = createAsyncThunk(
+  'product/getCategories',
+  async () => {
+    const response = await axios.get(
+      'https://fakestoreapi.com/products/categories'
+    );
     return response.data;
   }
 );
@@ -25,6 +46,9 @@ const productSlice = createSlice({
     setSelected: (state, action) => {
       state.selected = action.payload;
     },
+    setCategories: (state, action) => {
+      state.categories = action.payload;
+    },
     setLoading: (state, action) => {
       state.loading = action.payload;
     },
@@ -38,11 +62,26 @@ const productSlice = createSlice({
       .addCase(fetchProducts.fulfilled, (state, action) => {
         state.loading = false;
         state.products = action.payload;
+      })
+      .addCase(fetchWithCategorie.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchWithCategorie.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+      })
+      .addCase(getCategories.fulfilled, (state, action) => {
+        state.categories = action.payload;
       });
   },
 });
 
-export const { setProducts, setSelected, setLoading, resetState } =
-  productSlice.actions;
+export const {
+  setProducts,
+  setSelected,
+  setCategories,
+  setLoading,
+  resetState,
+} = productSlice.actions;
 
 export default productSlice.reducer;
